ff5b25da778d56eba9e61bb94567a12f004ebe03
Patrick Brockmann <Patrick_Brockmann@hotmail.com>
2022-11-26 11:50:51 +0100

    add energinet helpers

---
+++ git-prompt-energinet.sh
@@ -0,0 +1,10 @@
+# Energinet
+alias rs='yarn start'
+alias rsd='yarn nx run ui-watt:storybook'
+alias i='yarn install'
+
+alias rt='yarn nx run app-dh:test'
+alias rt:id='yarn nx run dh-metering-point-feature-identity-and-master-data:test --watch'
+alias rt:overview='yarn nx run dh-metering-point-feature-overview:test --watch'
+alias rt:search='yarn nx run dh-metering-point-feature-search:test --watch'
+alias rt:tabs='yarn nx run dh-metering-point-feature-tabs:test --watch'
