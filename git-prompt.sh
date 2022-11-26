7057271276fdc60e1ce8af066f6a642c276abe34
Patrick Brockmann <Patrick_Brockmann@hotmail.com>
2022-11-26 19:24:06 +0100

    Update git-prompt.sh

--- git-prompt.sh
+++ git-prompt.sh
@@ -1,60 +1,51 @@
-if test -f /etc/profile.d/git-sdk.sh
-then
+if test -f /etc/profile.d/git-sdk.sh; then
 	TITLEPREFIX=SDK-${MSYSTEM#MINGW}
 else
 	TITLEPREFIX=$MSYSTEM
 fi
 
-if test -f ~/.config/git/git-prompt.sh
-then
+if test -f ~/.config/git/git-prompt.sh; then
 	. ~/.config/git/git-prompt.sh
 else
 	PS1='\[\033]0;$TITLEPREFIX:$PWD\007\]' # set window title
-	PS1="$PS1"'\n'                 # new line
+	PS1="$PS1"'\n'                         # new line
 	# PS1="$PS1"'\[\033[32m\]'       # change to green
 	# PS1="$PS1"'Brockmann '             # user@host<space>
 	# PS1="$PS1"'\[\033[35m\]'       # change to purple
 	# PS1="$PS1"'$MSYSTEM '          # show MSYSTEM
-	PS1="$PS1"'\[\033[33m\]'       # change to brownish yellow
-	PS1="$PS1"'\W'                 # current working directory
-	if test -z "$WINELOADERNOEXEC"
-	then
+	PS1="$PS1"'\[\033[33m\]' # change to brownish yellow
+	PS1="$PS1"'\W'           # current working directory
+	if test -z "$WINELOADERNOEXEC"; then
 		GIT_EXEC_PATH="$(git --exec-path 2>/dev/null)"
 		COMPLETION_PATH="${GIT_EXEC_PATH%/libexec/git-core}"
 		COMPLETION_PATH="${COMPLETION_PATH%/lib/git-core}"
 		COMPLETION_PATH="$COMPLETION_PATH/share/git/completion"
-		if test -f "$COMPLETION_PATH/git-prompt.sh"
-		then
+		if test -f "$COMPLETION_PATH/git-prompt.sh"; then
 			. "$COMPLETION_PATH/git-completion.bash"
 			. "$COMPLETION_PATH/git-prompt.sh"
-			PS1="$PS1"'\[\033[36m\]'  # change color to cyan
-			PS1="$PS1"'`__git_ps1`'   # bash function
+			PS1="$PS1"'\[\033[36m\]' # change color to cyan
+			PS1="$PS1"'`__git_ps1`'  # bash function
 		fi
 	fi
-	PS1="$PS1"'\[\033[0m\]'        # change color
-	PS1="$PS1"'\n'                 # new line
-	PS1="$PS1"'$ '                 # prompt: always $
+	PS1="$PS1"'\[\033[0m\]' # change color
+	PS1="$PS1"'\n'          # new line
+	PS1="$PS1"'$ '          # prompt: always $
 fi
 
-MSYS2_PS1="$PS1"               # for detection by MSYS2 SDK's bash.basrc
+MSYS2_PS1="$PS1" # for detection by MSYS2 SDK's bash.basrc
 
 # Evaluate all user-specific Bash completion scripts (if any)
-if test -z "$WINELOADERNOEXEC"
-then
-	for c in "$HOME"/bash_completion.d/*.bash
-	do
+if test -z "$WINELOADERNOEXEC"; then
+	for c in "$HOME"/bash_completion.d/*.bash; do
 		# Handle absence of any scripts (or the folder) gracefully
 		test ! -f "$c" ||
-		. "$c"
+			. "$c"
 	done
 fi
 
 alias c='clear'
-alias cdgf='cd ~/Projects/greenforce-frontend'
 
-alias fe='code ~/Projects/greenforce-frontend'
 alias gitc='code (cd ../../Program\ Files/Git/etc/profile.d/git-prompt)'
-
 
 # Github
 alias com='git checkout main && git pull origin main'
@@ -70,3 +61,24 @@ alias apply='git stash apply'
 
 alias branch='git branch'
 
+
+# Package
+packageManager='npm start'
+packageManagerDev='npm run dev'
+install='npm install'
+
+set_env() {
+	if command -v 'yarn start'; then
+		packageManager='yarn start'
+		packageManagerDev='yarn start'
+		install='yarn install'
+	else
+		packageManager='npm start'
+		packageManagerDev='npm run dev'
+		install='npm install'
+	fi
+}
+
+alias rs=$packageManager
+alias rsd=$packageManagerDev
+alias i=$install
