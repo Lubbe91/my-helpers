d2c19a4ef18fda58b9152663356ea5fed29f93e8
Patrick Brockmann <Patrick_Brockmann@hotmail.com>
2022-11-26 19:24:27 +0100

    Update git-prompt-energinet.sh

--- git-prompt-energinet.sh
+++ git-prompt-energinet.sh
@@ -1,8 +1,8 @@
 # Energinet
-alias rs='yarn start'
-alias rsd='yarn nx run ui-watt:storybook'
-alias i='yarn install'
+alias cdgf='cd ~/Projects/greenforce-frontend'
+alias fe='code ~/Projects/greenforce-frontend'
 
+alias rsstory='yarn nx run ui-watt:storybook'
 alias rt='yarn nx run app-dh:test'
 alias rt:id='yarn nx run dh-metering-point-feature-identity-and-master-data:test --watch'
 alias rt:overview='yarn nx run dh-metering-point-feature-overview:test --watch'
